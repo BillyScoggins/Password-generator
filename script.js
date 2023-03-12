5a815a78b300a6898c894cc929a31e0872a7c912
Billy Scoggins <118499026+BillyScoggins@users.noreply.github.com>
2023-03-12 09:16:31 -0500

    variables

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var passwordLength;
+var confirmUpperCase;
+var confirmLowerCase;
+var confirmSymbols;
+var confirmNumbers;
+var userChoices;
+
+passwordLength = prompt("How long would you like your password to be?")
+console.log("password variable", passwordLength)
+
+var lowerCase = "abcdefghijklmnopqrstuvwxyz".split("")
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
+var Symbols = "!@#$%^&*+=?".split("")
+var Numbers = "0123456789".split("")
+
+
+console.log(upperCase)
+console.log(lowerCase)
+console.log(Symbols)
+console.log(Numbers)
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
